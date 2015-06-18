b5b967fa85adbfd80682a5c18cb40341df721a26
Joan Saez <janisaez@gmail.com>
2015-06-18 22:34:13 +0100

    node server error fixed

--- web.js
+++ web.js
@@ -1,1 +1,8 @@
-web: node web.js
+var gzippo = require('gzippo');
+var express = require('express');
+var morgan = require('morgan');
+var app = express();
+
+app.use(morgan('dev'));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.listen(process.env.PORT || 5000);
